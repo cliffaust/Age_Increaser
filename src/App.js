195f3a9df12e9e42bf18fb7ea2c663758eb64b7c
cliffaust <kingsleyclifford2016@gmail.com>
2021-03-27 09:15:39 +0000

    add lines of code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [state, changeAge] = useState({ age: 0 });
+
+  let btnStyles = {
+    padding: "12px 8px",
+    backgroundColor: "#F3A712",
+    fontWeight: 600,
+    borderRadius: "5px",
+    marginLeft: "5px",
+    cursor: "pointer",
+    border: "none",
+  };
+
+  const handleClick = () => {
+    changeAge({ age: state.age + 1 });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Age is {state.age}</h1>
+      <button onClick={handleClick} style={btnStyles}>
+        Click Me!!
+      </button>
     </div>
   );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = { age: 0 };
+//   }
+
+//   handleClick = () => {
+//     this.setState({ age: this.state.age + 1 });
+//   };
+
+//   render() {
+//     let btnStyles = {
+//       padding: "12px 8px",
+//       backgroundColor: "#F3A712",
+//       fontWeight: 600,
+//       borderRadius: "5px",
+//       marginLeft: "5px",
+//       cursor: "pointer",
+//       border: "none",
+//     };
+//     return (
+//       <div>
+//         <h1>Age is {this.state.age}</h1>
+//         <button onClick={this.handleClick} style={btnStyles}>
+//           Click Me!!
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
